4e6b45a392b6c10f20faf4e6d565b63175008861
BTu-newbiecod <tn150938@gmail.com>
2026-05-22 05:46:50 +0700

    feat:fix conflict

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8081/api/v1",
-=======
     baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8081/api/v1"
->>>>>>> origin/main
 });
 
 api.interceptors.request.use((config) => {
